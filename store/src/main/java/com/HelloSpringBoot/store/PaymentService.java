f910fa89d9042b7177c45c56bcffc93f0cdb24df
PoojaMulaguri593 <poojamulaguri2000@gmail.com>
2025-06-22 20:35:48 -0700

    My first Commit

---
+++ store/src/main/java/com/HelloSpringBoot/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.HelloSpringBoot.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
---
+++ store/src/main/java/com/HelloSpringBoot/store/StripePaymentService.java
@@ -0,0 +1,13 @@
+package com.HelloSpringBoot.store;
+
+import org.springframework.stereotype.Service;
+
+
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount){
+        System.out.println("Stripe");
+        System.out.println("Amount:"+amount);
+
+    }
+}
